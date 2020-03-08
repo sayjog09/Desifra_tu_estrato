e7be82030623d1e7046cb56e9b4420611c90c775
sayjog09 <49254127+sayjog09@users.noreply.github.com>
2020-03-08 14:15:17 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/desifra_tu_estrato/Main9Activity.java
@@ -0,0 +1,14 @@
+package com.example.desifra_tu_estrato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main9Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main9);
+    }
+}
